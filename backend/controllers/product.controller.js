d47fe9efae77615825a01ba0e0dc248a29f3ce4e
chamodhlv <chamodh300@gmail.com>
2026-06-13 20:17:47 +0530

    fix import statements for Product and User models to match casing

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.model.js";
+import Product from "../models/Product.model.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -8,4 +8,19 @@ export const getAllProducts = async (req, res) => {
     console.error("Error fetching products:", error);
     res.status(500).json({ message: "Server error" });
   }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    const featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json({ products: JSON.parse(featuredProducts) });
+    }
+
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+  } catch (error) {}
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", protectRoute, getFeaturedProducts);
 
 export default router;
