478ec972e4721e8a0d16d2cbcd76e6f5f45d3d0c
chamodhlv <chamodh300@gmail.com>
2026-06-16 14:55:37 +0530

    add cart functionality; implement cart routes and controller methods

---
+++ backend/controllers/cart.controller.js
@@ -0,0 +1,6 @@
+export const addToCart = (req, res) => {};
+
+export const getCartProducts = (req, res) => {};
+
+export const removeAllFromCart = (req, res) => {};
+export const updateQuantity = (req, res) => {};
